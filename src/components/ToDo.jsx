5c4a7fc44dff7ed2f0c7a5f02d90aa35c0c3af9e
LuchoBre <lucho.bregoli@gmail.com>
2023-09-18 16:37:43 -0300

    se le modificaron iconos y estilo de letra

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,57 +1,79 @@
-import { useState } from 'react'
-import '../assets/styles/toDo.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
+import "../assets/styles/toDo.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFileCirclePlus,
+  faPenToSquare,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 
 const ToDo = () => {
-const [task, setTask] = useState('')
-const [list, setList] = useState([])
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
 
-function addTask () {
+  function addTask() {
     setList([...list, task]);
-    setTask('')
-}
+    setTask("");
+  }
 
-function removeTask (index) {
+  function removeTask(index) {
     const newList = [...list];
     newList.splice(index, 1);
-    setList(newList)
-}
+    setList(newList);
+  }
 
-function editTask (listItem, index) {
-    setTask(listItem)
-    list.splice(index, 1)    
-}
+  function editTask(listItem, index) {
+    setTask(listItem);
+    list.splice(index, 1);
+  }
 
   return (
     <>
-    <div className="block-container">
+      <div className="block-container">
         <div className="todo-container">
-            <h1>To Do List</h1>
-                <div className='input-container'>
-                    <input 
-                    type="text" 
-                    value={task} 
-                    onChange={(e) => setTask(e.target.value)}
-                    />
-                    <FontAwesomeIcon icon={faFileCirclePlus} style={{color: "#ffffff",}} onClick={addTask} /> 
+          <h1>To Do List</h1>
+          <div className="input-container">
+            <input
+              type="text"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
+            <FontAwesomeIcon
+              icon={faFileCirclePlus}
+              size="2xl"
+              id="add"
+              style={{ color: "#ffffff" }}
+              onClick={addTask}
+            />
+          </div>
+          <ul className="list-container">
+            {list.map((listItem, index) => (
+              <li key={index} className="list-data">
+                <input type="checkbox" name="" id="" />
+                <p>{listItem.charAt(0).toUpperCase() + listItem.slice(1).toLowerCase()}</p>
+                <div className="buttons-data">
+                  <FontAwesomeIcon
+                    icon={faPenToSquare}
+                    size="2xl"
+                    style={{ color: "#ffffff" }}
+                    id="edit"
+                    onClick={() => editTask(listItem, index)}
+                  />
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    size="2xl"
+                    style={{ color: "#c91f13" }}
+                    id="remove"
+                    onClick={() => removeTask(index)}
+                  />
                 </div>
-            <ul className="list-container">
-                {list.map((listItem, index) => (
-                    <li key={index} className='list-data'>
-                        <input type="checkbox" name="" id="" />
-                        <p>{listItem}</p>
-                        <div className="buttons-data">
-                        <button id='edit' onClick={()=> editTask(listItem, index)}>Edit</button>
-                        <button id='remove' onClick={() => removeTask(index)}>Remove</button>
-                    </div>
-                    </li>
-                ))}
-            </ul>
+              </li>
+            ))}
+          </ul>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ToDo
+export default ToDo;
