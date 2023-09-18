8c4ec4c22bd2d5685c4f350d865a026472ec838a
Gaston <gastonsalaya@gmail.com>
2023-09-18 16:37:40 -0300

    Rama Gaston. Se agregaron comentarios. Se cambió el tamaño del botón de agregar tareas.

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,57 +1,62 @@
-import { useState } from 'react'
-import '../assets/styles/toDo.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react'; // Importa useState desde React para gestionar el estado
+import '../assets/styles/toDo.css'; // Importa estilos CSS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importa FontAwesome para los iconos
+import { faFileCirclePlus } from '@fortawesome/free-solid-svg-icons'; // Importa un icono específico
 
 const ToDo = () => {
-const [task, setTask] = useState('')
-const [list, setList] = useState([])
+  const [task, setTask] = useState(''); // Estado para la tarea actual
+  const [list, setList] = useState([]); // Estado para la lista de tareas
 
-function addTask () {
+  // Función para agregar una tarea a la lista
+  function addTask() {
     setList([...list, task]);
-    setTask('')
-}
+    setTask(''); // Limpia el campo de entrada después de agregar
+  }
 
-function removeTask (index) {
+  // Función para eliminar una tarea de la lista
+  function removeTask(index) {
     const newList = [...list];
     newList.splice(index, 1);
-    setList(newList)
-}
+    setList(newList);
+  }
 
-function editTask (listItem, index) {
-    setTask(listItem)
-    list.splice(index, 1)    
-}
+  // Función para editar una tarea
+  function editTask(listItem, index) {
+    setTask(listItem);
+    list.splice(index, 1); // Elimina la tarea de la lista mientras se edita
+  }
 
   return (
     <>
-    <div className="block-container">
+      <div className="block-container">
         <div className="todo-container">
-            <h1>To Do List</h1>
-                <div className='input-container'>
-                    <input 
-                    type="text" 
-                    value={task} 
-                    onChange={(e) => setTask(e.target.value)}
-                    />
-                    <FontAwesomeIcon icon={faFileCirclePlus} style={{color: "#ffffff",}} onClick={addTask} /> 
+          <h1>To Do List</h1> {/* Título de la lista de tareas */}
+          <div className='input-container'>
+            <input
+              type="text"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
+            <FontAwesomeIcon icon={faFileCirclePlus} style={{ color: "#ffffff", fontSize: '7em', marginTop: '50px' }} onClick={addTask} /> 
+            {/* Icono FontAwesome para agregar tareas */}
+          </div>
+          <ul className="list-container">
+            {/* Itera sobre la lista de tareas y muestra cada elemento */}
+            {list.map((listItem, index) => (
+              <li key={index} className='list-data'>
+                <input type="checkbox" name="" id="" /> {/* Checkbox */}
+                <p>{listItem}</p> {/* Muestra el texto de la tarea */}
+                <div className="buttons-data">
+                  <button id='edit' onClick={() => editTask(listItem, index)}>Editar</button> {/* Botón para editar tarea */}
+                  <button id='remove' onClick={() => removeTask(index)}>Eliminar</button> {/* Botón para eliminar tarea */}
                 </div>
-            <ul className="list-container">
-                {list.map((listItem, index) => (
-                    <li key={index} className='list-data'>
-                        <input type="checkbox" name="" id="" />
-                        <p>{listItem}</p>
-                        <div className="buttons-data">
-                        <button id='edit' onClick={()=> editTask(listItem, index)}>Edit</button>
-                        <button id='remove' onClick={() => removeTask(index)}>Remove</button>
-                    </div>
-                    </li>
-                ))}
-            </ul>
+              </li>
+            ))}
+          </ul>
         </div>
-    </div>
+      </div>
     </>
   )
 }
 
-export default ToDo
+export default ToDo; // Exporta el componente ToDo
